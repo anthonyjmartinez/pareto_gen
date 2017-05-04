e52d3ca0728f3418fd50bede8379ac5c9baf3842
Anthony Martinez <anthony.martinez@gmail.com>
2017-05-04 16:33:26 -0500

    Create distutils module package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Pareto Gen',
+    version='0.1',
+    description='Python Pareto Generator using Pandas, NumPy, and Bokeh',
+    author='Anthony Martinez',
+    author_email='anthony.martinez@gmail.com',
+    url='https://github.com/anthonyjmartinez/pareto_gen',
+    py_modules=['pareto_gen'])
